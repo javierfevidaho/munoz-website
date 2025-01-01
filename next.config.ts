1f538bcecacacf7db3bbd7b984c3832de8f8b9fa
Javier Felix <javierfevidaho@gmail.com>
2024-12-31 23:22:27 -0700

    final

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true, // Ignora ESLint durante el build
   },
+  output: 'standalone', // Hace que la aplicación sea más compatible con entornos de despliegue
 };
 
 module.exports = nextConfig;
